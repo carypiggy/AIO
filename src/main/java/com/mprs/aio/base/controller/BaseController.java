a603696dc751c5b3e9550d433cf05b0ea00f6c91
Cary <carypiggy@gmail.com>
2018-07-31 17:15:25 +0800

    baseController调整

--- src/main/java/com/mprs/aio/base/controller/BaseController.java
+++ src/main/java/com/mprs/aio/base/controller/BaseController.java
@@ -42,24 +42,6 @@ public class BaseController {
 	protected Logger logger = LoggerFactory.getLogger(getClass());
 
 	/**
-	 * 管理基础路径
-	 */
-	@Value("${adminPath}")
-	protected String adminPath;
-	
-	/**
-	 * 前端基础路径
-	 */
-	@Value("${frontPath}")
-	protected String frontPath;
-	
-	/**
-	 * 前端URL后缀
-	 */
-	@Value("${urlSuffix}")
-	protected String urlSuffix;
-	
-	/**
 	 * 验证Bean实例对象
 	 */
 	@Autowired
--- src/main/java/com/mprs/aio/system/controller/SysUserController.java
+++ src/main/java/com/mprs/aio/system/controller/SysUserController.java
@@ -1,12 +1,23 @@
 package com.mprs.aio.system.controller;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.mprs.aio.base.controller.BaseController;
+import com.mprs.aio.system.service.SysUserService;
 
-//@RestController
-//@EnableAutoConfiguration
+
+
+@Controller
+@RequestMapping("/sys/sysuser")
 public class SysUserController extends BaseController {
 
+	@Autowired
+	private SysUserService sysUserService;
+	
+	
+	
+	
+	
 }
