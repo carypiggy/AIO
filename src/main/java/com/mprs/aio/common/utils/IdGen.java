5ece59d1547adba6704b7c5aee4e6a8c203e2a8f
Cary <carypiggy@gmail.com>
2018-08-01 17:17:43 +0800

    字段类型调整

--- src/main/java/com/mprs/aio/common/utils/IdGen.java
+++ src/main/java/com/mprs/aio/common/utils/IdGen.java
@@ -1,6 +1,3 @@
-/**
- * Copyright &copy; 2012-2016 <a href="https://github.com/thinkgem/jeesite">JeeSite</a> All rights reserved.
- */
 package com.mprs.aio.common.utils;
 
 import java.io.Serializable;
