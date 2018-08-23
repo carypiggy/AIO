6cc59bf7df9c8d37bc70a81634b28ad158624e48
Cary <carypiggy@gmail.com>
2018-08-23 14:52:37 +0800

    还原TEST方法

---
+++ AIO_SER/src/test/java/com/mpri/aio/gen/PingyinTest.java
@@ -0,0 +1,17 @@
+package com.mpri.aio.gen;
+
+import com.mpri.aio.gen.util.PinyinUtil;
+import com.mpri.aio.gen.util.PinyinUtil.Type;
+
+/**
+* desc
+* @author lzq
+* @date 2018年8月20日 - 下午2:00:43
+*/
+public class PingyinTest {
+
+	 public static void main(String[] args) throws Exception{
+		 	PinyinUtil tool = new PinyinUtil();
+//			System.out.println("刘亚壮的运行测试结果为====" + tool.toPinYin("刘亚壮",  Type.LOWERCASE));
+		}
+}
