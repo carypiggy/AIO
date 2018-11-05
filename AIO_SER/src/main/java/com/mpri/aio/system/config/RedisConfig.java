3035e57522245ff98c73afdee23179b706bf7ce0
Cary <carypiggy@gmail.com>
2018-11-05 13:06:44 +0800

    关闭redis缓存

--- AIO_SER/src/main/java/com/mpri/aio/system/config/RedisConfig.java
+++ AIO_SER/src/main/java/com/mpri/aio/system/config/RedisConfig.java
@@ -21,7 +21,7 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-@Configuration
+//@Configuration
 public class RedisConfig {
 	
 	@Bean
