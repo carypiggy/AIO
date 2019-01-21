47f737776d9c51605e0062b130f22f837e6b0892
Cary <carypiggy@gmail.com>
2019-01-21 16:35:22 +0800

    更名

---
+++ aio_cloud/src/test/java/com/mpri/aio/aio_cloud/AioCloudApplicationTests.java
@@ -0,0 +1,17 @@
+package com.mpri.aio.aio_cloud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AioCloudApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
