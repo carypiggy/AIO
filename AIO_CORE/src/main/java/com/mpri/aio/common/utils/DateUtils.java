dab082c7419e8d61834a04d6e66b3534a34b5bf4
Cary <carypiggy@gmail.com>
2019-12-10 08:53:11 +0800

    Update DateUtils.java

--- AIO_CORE/src/main/java/com/mpri/aio/common/utils/DateUtils.java
+++ AIO_CORE/src/main/java/com/mpri/aio/common/utils/DateUtils.java
@@ -191,6 +191,21 @@ public class DateUtils extends org.apache.commons.lang3.time.DateUtils {
 		}
 		return i;
 	}
+	
+	/**
+	 * 通过日期获取星期
+	 * @param date
+	 * @return
+	 */
+	public static String getWeekOfDate(Date date) {
+		String[] weekDays = { "星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六" };
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(date);
+		int w = cal.get(Calendar.DAY_OF_WEEK) - 1;
+		if (w < 0)
+			w = 0;
+		return weekDays[w];
+	}
 	/**
 	 * @param args
 	 * @throws ParseException
